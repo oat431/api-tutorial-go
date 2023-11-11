3e1ebee90134fe4645b61a606ec8542d34d9d7f3
oat431 <oat431@gmail.com>
2023-11-12 00:56:51 +0700

    🔨 add: fully graphql

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.20
 require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/graphql-go/graphql v0.8.1
+	github.com/graphql-go/handler v0.2.3
+	github.com/morkid/paginate v1.1.7
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -20,9 +23,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/graphql-go/graphql v0.8.1 // indirect
 	github.com/graphql-go/graphql-go-handler v0.2.3 // indirect
-	github.com/graphql-go/handler v0.2.3 // indirect
 	github.com/iancoleman/strcase v0.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -37,7 +38,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morkid/gocache v1.0.0 // indirect
-	github.com/morkid/paginate v1.1.7 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
--- resolver/user_resolver.go
+++ resolver/user_resolver.go
@@ -1,22 +1,24 @@
 package resolver
 
 import (
-	"strconv"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
 	"github.com/graphql-go/graphql"
+	"github.com/oat431/api-tutorial-go/payload/request"
 	"github.com/oat431/api-tutorial-go/payload/response"
 	"github.com/oat431/api-tutorial-go/services"
 )
 
 type UserResolver interface {
-	GetAllDBUsers(params graphql.ResolveParams) (interface{},error)
-	GetAllDBUsersPagination(params graphql.ResolveParams) (interface{},error)
-	GetAllDBUsersById(params graphql.ResolveParams) (interface{},error)
-	// CreateUser(params graphql.ResolveParams) (interface{}, error)
-	// UpdateUser(params graphql.ResolveParams) (interface{}, error)
-	// DeleteUser(params graphql.ResolveParams) (interface{}, error)
+	GetAllDBUsers(params graphql.ResolveParams) (interface{}, error)
+	GetAllDBUsersPagination(params graphql.ResolveParams) (interface{}, error)
+	GetAllDBUsersById(params graphql.ResolveParams) (interface{}, error)
+	CreateUser(params graphql.ResolveParams) (interface{}, error)
+	UpdateUser(params graphql.ResolveParams) (interface{}, error)
+	DeleteUser(params graphql.ResolveParams) (interface{}, error)
 }
 
 type userService struct {
@@ -27,32 +29,51 @@ func CreateUserResolver(service services.UserV2Service) *userService {
 	return &userService{service}
 }
 
-func (s *userService) GetAllDBUsers(params graphql.ResolveParams) (interface{},error) {
+func (s *userService) GetAllDBUsers(params graphql.ResolveParams) (interface{}, error) {
 	users := s.service.GetAllDBUsers()
 	return users, nil
 }
 
-func (s *userService) GetAllDBUsersPagination(params graphql.ResolveParams) (interface{},error) {
+func (s *userService) GetAllDBUsersPagination(params graphql.ResolveParams) (interface{}, error) {
 	page := params.Args["page"].(int)
 	size := params.Args["limit"].(int)
-	
+
 	if page <= 0 {
 		page = 1
 	} else {
 		page = page - 1
 	}
 
-	url := "https://try-gin-deploy.onrender.com/api/v2/users/pages?page=" + strconv.Itoa(page) + "&size=" +strconv.Itoa(size)
+	url := "https://try-gin-deploy.onrender.com/api/v2/users/pages?page=" + strconv.Itoa(page) + "&size=" + strconv.Itoa(size)
 	req, _ := http.NewRequest("GET", url, nil)
 	res, _ := http.DefaultClient.Do(req)
 	body, _ := ioutil.ReadAll(res.Body)
 	var data response.PageUserDto
 	json.Unmarshal(body, &data)
-	return data,nil;
+	return data, nil
 }
 
-func (s *userService) GetAllDBUsersById(params graphql.ResolveParams) (interface{},error) {
+func (s *userService) GetAllDBUsersById(params graphql.ResolveParams) (interface{}, error) {
 	id := params.Args["id"].(int)
 	user := s.service.GetAllDBUsersById(strconv.Itoa(id))
-	return user,nil
+	return user, nil
+}
+
+func (s *userService) CreateUser(params graphql.ResolveParams) (interface{}, error) {
+	userRequest := params.Args["request"].(request.UserRequest)
+	userDto := s.service.CreateUser(userRequest)
+	return userDto, nil
+}
+
+func (s *userService) UpdateUser(params graphql.ResolveParams) (interface{}, error) {
+	id := params.Args["id"].(int)
+	userRequest := params.Args["request"].(request.UserRequest)
+	userDto := s.service.UpdateUser(strconv.Itoa(id), userRequest)
+	return userDto, nil
+}
+
+func (s *userService) DeleteUser(params graphql.ResolveParams) (interface{}, error) {
+	id := params.Args["id"].(int)
+	s.service.DeleteUser(strconv.Itoa(id))
+	return "User deleted", nil
 }
